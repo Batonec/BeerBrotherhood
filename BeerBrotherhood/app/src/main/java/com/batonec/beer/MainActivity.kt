6f6e4960264e3f562bb64033b46e1036173010db
Batonec <pasapaval@gmail.com>
2023-12-06 22:17:01 +0300

    edge to edge

--- BeerBrotherhood/app/src/main/java/com/batonec/beer/MainActivity.kt
+++ BeerBrotherhood/app/src/main/java/com/batonec/beer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.batonec.beer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +16,7 @@ import com.batonec.beer.ui.theme.BeerBrotherhoodTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             BeerBrotherhoodTheme {
